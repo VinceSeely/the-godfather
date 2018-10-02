4750e1a1b9f666b994cd9bbd08ccd56cdd6a4290
Dakota J Methvin <methvind@uwplatt.edu>
2018-10-02 13:17:52 -0500

    Initial class and unit test setup for R6S Module.

---
+++ TheGodfatherTests/Modules/R6Siege/R6SiegeStatsModuleTest.cs
@@ -0,0 +1,28 @@
+﻿// Author: Dakota Methvin
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace TheGodfatherTests.Modules.R6Siege
+{
+    [TestFixture]
+    public class R6SiegeStatsModuleTest
+    {
+        [SetUp]
+        public void temp1()
+        {
+
+        }
+        
+        [Test]
+        public void temp()
+        {
+
+        }
+
+
+
+    }
+}
