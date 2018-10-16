3daf6dcfcfdcac670a233d52a010c349e95af487
Dakota J Methvin <methvind@uwplatt.edu>
2018-10-16 18:43:13 -0500

    API added and modified to support offline work.

--- TheGodfather/Modules/R6Siege/R6SiegeStatsModule.cs
+++ TheGodfather/Modules/R6Siege/R6SiegeStatsModule.cs
@@ -4,6 +4,7 @@ using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
 
 using System;
 using System.Collections.Generic;
@@ -11,28 +12,39 @@ using System.Text;
 using System.Threading.Tasks;
 
 using TheGodfather.Common;
+
 using TheGodfather.Common.Attributes;
+using TheGodfather.Services;
 
 
 namespace TheGodfather.Modules.R6Siege
 {
+    using static R6SiegeAPI.API;
+    using static R6SiegeAPI.Enums;
+    using static R6SiegeAPI.Models;
+
     [Group("siege"), Module(ModuleType.R6Siege), NotBlocked]
     [Description("View R6S player statistics.")]
     [Cooldown(1, 1, CooldownBucketType.Channel)] // NumUses, CooldownTime, CooldownType
-    [UsageExamples("!r6stats")]
 
-
-    public class R6SiegeStatsModule
+    public class R6SiegeStatsModule : TheGodfatherModule
     {
 
-        [Command("temp")]
+        public R6SiegeStatsModule(SharedData shared, DBService db)
+            : base (shared, db)
+        {
+            this.ModuleColor = DiscordColor.HotPink;
+        }
+
+
+        [Command("r6stats")]
         [Description("This command has no functionality, it is merely a temporary prototype.")]
-        [Aliases("t", "tmp")]
-        [UsageExamples("!temp", 
-                       "!r6stats temp @someone")]
+        [Aliases("r6", "6stats")]
+        [UsageExamples("!r6stats playername")]
         public async Task temp(CommandContext ctx)
         {
-
+            
+            
         }
 
     }
